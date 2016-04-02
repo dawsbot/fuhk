ca99b1703b2de0e71f749e6b49a512aac7bcd9bb
Dawson Botsford <DawsonBotsford@gmail.com>
2016-04-02 10:42:06 -0600

    Revert "Allow sub-matches of badwords"
    This reverts commit f87fdb276d08634779f877adc8b8185d6390dfe6.

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 const badWords = require('./swearWords.json');
 
 module.exports = function (str) {
+  const argLength = arguments.length;
+
   // validate arguments
   if (typeof str !== 'string') {
     throw new TypeError(`Expected an object, got ${typeof str}`);
   }
-  const reg = new RegExp(badWords.join('|'), 'gi');
+  if (argLength !== 1) {
+    throw new Error(`Expected 1 arguments, got ${argLength}`);
+  }
+
+  const reg = new RegExp('\\b' + badWords.join('\\b|\\b') + '\\b', 'gi');
 
   return str.match(reg) || [];
 };
